ae8d1f3bfdd9e7382c87712b185ec9c37c758e61
Wes <wmont012@ucr.edu>
2022-02-04 23:23:24 -0800

    alan can MOVE >:0

---
+++ Interdimensional Supermarket/Assets/Scripts/Alan.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alan : MonoBehaviour
+{
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t=gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("up")){
+            t.position+= new Vector3(0,1);
+        }
+        else if(Input.GetKeyDown("down")){
+            t.position+= new Vector3(0,-1);
+        }
+        else if(Input.GetKeyDown("right")){
+            t.position+= new Vector3(1,0);
+        }
+        else if(Input.GetKeyDown("left")){
+            t.position+= new Vector3(-1,0);
+        }
+    }
+}
