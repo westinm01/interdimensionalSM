ead345c0333693170aa0f5c7291925e80138f36d
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2022-02-06 00:07:30 -0800

    fixed powerups

--- Interdimensional Supermarket/Assets/BoltScript.cs
+++ Interdimensional Supermarket/Assets/BoltScript.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class BoltScript : Powerup
 {
+    private AudioSource audio;
+
+    void Awake() {
+        audio = GameObject.FindGameObjectWithTag("BoltSound").GetComponent<AudioSource>();
+    }
+
     public override void Use(){
         GameObject[] g = GameObject.FindGameObjectsWithTag("Customer");
         foreach(GameObject e in g) {
             e.transform.position = new Vector2(0, 0);
         }
+        audio.Play();
         Destroy(this.gameObject);
+      
+
     }
 }
